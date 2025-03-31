2addec278753b0ba3da3eea4426254ba515d0ccb
EricBernardi <eric.gomes.bernardi@gmail.com>
2025-03-31 00:57:33 -0300

    feat: create DTO Person class

--- rest-with-spring-boot-and-java/src/main/java/br/com/eric/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/eric/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package br.com.eric.controllers;
 
-import br.com.eric.model.Person;
+import br.com.eric.data.dto.PersonDTO;
 import br.com.eric.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,14 +17,14 @@ public class PersonController {
     private PersonService service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll (){
+    public List<PersonDTO> findAll (){
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById (@PathVariable("id") Long id){
+    public PersonDTO findById (@PathVariable("id") Long id){
         return service.findById(id);
     }
 
@@ -32,7 +32,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person register (@RequestBody Person person){
+    public PersonDTO register (@RequestBody PersonDTO person){
         return service.create(person);
     }
 
@@ -40,7 +40,7 @@ public class PersonController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person update (@RequestBody Person person){
+    public PersonDTO update (@RequestBody PersonDTO person){
         return service.updatePerson(person);
     }
 
